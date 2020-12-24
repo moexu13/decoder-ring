2fad3d8fc57ec99fb68e0d4b3521c73841d439bc
Melissa Albarella <moexu13@gmail.com>
2020-12-23 16:35:49 -0800

    implemented caeser cipher and tests

--- src/caesar.js
+++ src/caesar.js
@@ -1,3 +1,38 @@
-function caesar(input, shift, encode = true) {}
+function caesar(input, shift, encode = true) {
+  if (!shift || shift > 25 || shift < -25) return false;
+  if (!input.length) return "";
 
-module.exports = caesar;
+  // decoding changes the shift direction
+  if (!encode) shift = -shift;
+  input = input.toLowerCase();
+
+  let text = "";
+  for (let i = 0; i < input.length; i++) {
+    text += asciiMap(input.charCodeAt(i), shift);
+  }
+  return text;
+}
+
+function asciiMap(char, shift) {
+  // lower case ascii is from 97 to 122
+  const a = 97;
+  const z = 122;
+  // if what's passed in is something outside of this ascii range then just return it
+  if (char < a || char > z)  {
+    return String.fromCharCode(char);
+  } else {
+    if (char + shift < a) {
+      // if the char code falls below a loop around to z
+      // 96 should equal 122
+      return String.fromCharCode(z - ((a - (char + shift)) - 1));
+      // if the char code goes above z loop around to a
+      // 123 should equal
+    } else if (char + shift > z) {
+      return String.fromCharCode(a + ((char + shift) - z) - 1);
+    } else {
+      return String.fromCharCode(char + shift);
+    }
+  }
+}
+
+module.exports = { caesar, asciiMap };
--- test/caesar.test.js
+++ test/caesar.test.js
@@ -1,1 +1,75 @@
-// Write your tests here!
+const expect = require("chai").expect;
+
+const { caesar, asciiMap } = require("../src/caesar");
+
+describe("caeser", () => {
+  it("returns false if shift value is missing", () => {
+    const actual = caesar();
+    expect(actual).to.be.false;
+  });
+  it("returns false if shift value is 0", () => {
+    const actual = caesar("", 0);
+    expect(actual).to.be.false;
+  });
+  it("returns false if shift value is greater than 25", () => {
+    const actual = caesar("", 26);
+    expect(actual).to.be.false;
+  });
+  it("returns false if shift value is less than -25", () => {
+    const actual = caesar("", -27);
+    expect(actual).to.be.false;
+  });
+  it("should shift a provided string by the shift amount", () => {
+    const expected = "ibm";
+    const actual = caesar("hal", 1);
+    expect(actual).to.equal(expected);
+  });
+  it("should be case insensitive", () => {
+    const expected = "ibm";
+    const actual = caesar("HAL", 1);
+    expect(actual).to.equal(expected);
+  });
+  it("should preserve whitespace", () => {
+    const expected = "qrgp rqf";
+    const actual = caesar("open pod", 2);
+    expect(actual).to.equal(expected);
+  });
+  it("should preserve any nonalphabetic character", () => {
+    const expected = "@kruvh_herrnv";
+    const actual = caesar("@horse_ebooks", 3);
+    expect(actual).to.equal(expected);
+  });
+  it("should handle left shifts beyond a", () => {
+    const expected = "xifyxyx";
+    const actual = caesar("alibaba", -3);
+    expect(actual).to.equal(expected);
+  });
+  it("should handle shifts above z", () => {
+    const expected = "aoacss";
+    const actual = caesar("xlxzpp", 3);
+    expect(actual).to.equal(expected);
+  });
+  it("should properly decode message", () => {
+    const expected = "this is a secret message!";
+    const actual = caesar("BPQA qa I amkzmb umaaiom!", 8, false);
+    expect(actual).to.be.equal(expected);
+  });
+});
+
+describe("asciiMap", () => {
+  it("should return a character code's string representation", () => {
+    const expected = "a";
+    const actual = asciiMap(97, 0);
+    expect(actual).to.equal(expected);
+  });
+  it("should return a when given z and a shift of 1", () => {
+    const expected = "a";
+    const actual = asciiMap(122, 1);
+    expect(actual).to.equal(expected);
+  });
+  it("should return z when given a and a shift of -1", () => {
+    const expected = "z";
+    const actual = asciiMap(97, -1);
+    expect(actual).to.equal(expected);
+  });
+})
