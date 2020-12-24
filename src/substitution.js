0c496172bb9453429ec93c6bf1c69f3c1f230189
Melissa Albarella <moexu13@gmail.com>
2020-12-24 13:32:34 -0800

    implemented substitution cipher and tests

--- src/substitution.js
+++ src/substitution.js
@@ -1,3 +1,52 @@
-function substitution(input, alphabet, encode = true) {}
+const referenceAlphabet = "abcdefghijklmnopqrstuvwxyz";
+
+function substitution(input, alphabet, encode = true) {
+  if (alphabet.length !== 26 || !alphabetIsComplete(alphabet)) return false;
+  
+  input = input.toLowerCase();
+  if (input && encode) {
+    return encodeMessage(input, alphabet);
+  }
+  if(input && !encode) {
+    return decodeMessage(input, alphabet);
+  }
+}
+
+function alphabetIsComplete(alphabet) {
+  // verify that each letter of the alphabet is present with no duplicates
+  return [...alphabet].sort().join("") === [...referenceAlphabet].sort().join("");
+}
+
+function encodeMessage(input, alphabet) {
+  let output = "";
+  for(let i = 0; i < input.length; i++) {
+    // leave spaces as is
+    if (input[i] === " ") {
+      output += input[i];
+    } else {
+      // get index of character in alphabet
+      // get character at same index in provided alphabet
+      let index = referenceAlphabet.indexOf(input[i]);
+      output += alphabet[index];
+    }
+  }
+  return output;
+}
+
+function decodeMessage(input, alphabet) {
+  let output = "";
+  for(let i = 0; i < input.length; i++) {
+    // leave spaces as is
+    if (input[i] === " ") {
+      output += input[i];
+    } else {
+      // find the index in the provided alpabet
+      // then find the same index in the regular alphabet
+      let index = alphabet.indexOf(input[i]);
+      output += referenceAlphabet[index];
+    }
+  }
+  return output;
+}
 
 module.exports = substitution;
--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,1 +1,31 @@
-// Write your tests here!
+const expect = require("chai").expect;
+
+const substitution = require("../src/substitution");
+
+const alpha = "zyxwvutsrqponmlkjihgfedcba";
+
+describe("substitution", () => {
+  it("should return false if the alphabet parameter is not 26 characters", () => {
+    const actual = substitution("test", "abcdef");
+    expect(actual).to.be.false;
+  });
+  it("should maintain string whitespace", () => {
+    const expected = "mld rh gsv drmgvi lu lfi wrhxlmgvmg";
+    const actual = substitution("now is the winter of our discontent", alpha);
+    expect(actual).to.equal(expected);
+  });
+  it("is case insensitive", () => {
+    const expected = "gvhg gvhg";
+    const actual = substitution("TEST TEST", alpha);
+    expect(actual).to.equal(expected);
+  });
+  it("has a complete alphabet parameter", () => {
+    const actual = substitution("test", "abacadaeafagahaiajakalaman");
+    expect(actual).to.be.false;
+  });
+  it("can decode input", () => {
+    const expected = "thinkful";
+    const actual = substitution("jrufscpw", "xoyqmcgrukswaflnthdjpzibev", false);
+    expect(actual).to.equal(expected);
+  });
+});
