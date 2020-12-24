4cba2f6167b0f6a0970705671eae640dfc1b5266
Melissa Albarella <moexu13@gmail.com>
2020-12-23 18:37:26 -0800

    implemented polybius cipher and tests

--- src/polybius.js
+++ src/polybius.js
@@ -1,3 +1,69 @@
-function polybius(input, encode = true) {}
+const grid = [
+  {letter: "a", code: "11"},
+  {letter: "b", code: "21"},
+  {letter: "c", code: "31"},
+  {letter: "d", code: "41"},
+  {letter: "e", code: "51"},
+  {letter: "f", code: "12"},
+  {letter: "g", code: "22"},
+  {letter: "h", code: "32"},
+  {letter: "i", code: "42"},
+  {letter: "j", code: "42"},
+  {letter: "k", code: "52"},
+  {letter: "l", code: "13"},
+  {letter: "m", code: "23"},
+  {letter: "n", code: "33"},
+  {letter: "o", code: "43"},
+  {letter: "p", code: "53"},
+  {letter: "q", code: "14"},
+  {letter: "r", code: "24"},
+  {letter: "s", code: "34"},
+  {letter: "t", code: "44"},
+  {letter: "u", code: "54"},
+  {letter: "v", code: "15"},
+  {letter: "w", code: "25"},
+  {letter: "x", code: "35"},
+  {letter: "y", code: "45"},
+  {letter: "z", code: "55"},
+];
+
+function polybius(input, encode = true) {
+  let output = "";
+  // the letter codes are in pairs so a string to be decoded has to be even excluding whitespace
+  if (!encode && input.replace(/\s/g, "").length % 2 !== 0) {  
+    return false;
+  }
+  if (input && encode) {
+    input = input.toLowerCase();
+    for (let i = 0; i < input.length; i++) {
+      if (input[i] !== " ") { 
+        output += grid.find(item => item.letter === input[i]).code;
+      } else {
+        output += input[i];
+      }
+    }
+  }
+  if (input && !encode) {
+    // if the input contains spaces we don't want to use them when decoding
+    let encoded = input.split(" ");
+    encoded.forEach(chunk => {
+      // split numbers into pairs for decoding
+      let pairs = chunk.match(/\d{2}/g);
+      pairs.forEach(pair => {
+        // handle this special case separately
+        if (pair === "42") {
+          output += "(i/j)";
+        // everything else can be looked up
+        } else {
+          output += grid.find(item => item.code === pair).letter;
+        }
+      });
+      // put the spaces back
+      output += " ";
+    });
+  }
+  // get rid of any extra whitespace left over from decoding
+  return output.trim();
+}
 
 module.exports = polybius;
--- test/polybius.test.js
+++ test/polybius.test.js
@@ -1,1 +1,39 @@
-// Write your tests here!
+const expect = require("chai").expect;
+
+const polybius = require("../src/polybius");
+
+describe("polybius", () => {
+  it("should output a string when encoding", () => {
+    const actual = polybius("string");
+    expect(actual).to.be.a('string');
+  });
+  it("should return false if the string to be decoded minus whitespace isn't even", () => {
+    const actual = polybius("12345 ", false);
+    expect(actual).to.be.false;
+  });
+  it("should encode a single word", () => {
+    const expected = "311144";
+    const actual = polybius("cat");
+    expect(actual).to.equal(expected);
+  });
+  it("should leave whitespace untouched", () => {
+    const expected = "11 311144";
+    const actual = polybius("a cat");
+    expect(actual).to.equal(expected);
+  });
+  it("should be case insensitive", () => {
+    const expected = "11 311144";
+    const actual = polybius("A cAT");
+    expect(actual).to.equal(expected);
+  });
+  it("should decode a simple string", () => {
+    const expected = "cat";
+    const actual = polybius("311144", false);
+    expect(actual).to.equal(expected);
+  });
+  it("should decode 42 (i and j) as (i/j)", () => {
+    const expected = "th(i/j)s (i/j)s (i/j)t";
+    const actual = polybius("44324234 4234 4244", false);
+    expect(actual).to.equal(expected);
+  });
+});
